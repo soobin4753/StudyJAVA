a84024c617137a18676fd7f7841dddeedb67b9d6
soobin4753 <soobin4753@naver.com>
2022-07-13 21:57:28 +0900

    class

---
+++ src/Class/StudentExample.java
@@ -0,0 +1,16 @@
+package Class;
+
+public class StudentExample {
+
+	public static void main(String[] args) {
+		// 클래스로부터 객체 생성
+		
+		StudentExample s1 = new StudentExample();
+		System.out.println("s1 변수가 student 객체를 참조합니다");
+		
+		StudentExample s2 = new StudentExample();
+		System.out.println("s2 변수가 또 다른 student 객체를 참조합니다");
+
+	}
+
+}
